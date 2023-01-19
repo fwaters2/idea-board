ecb779e98ef11ad693037b6e38da6d01d00d3843
forrestwaters <forrestwaters@gmail.com>
2023-01-19 16:28:36 +0800

    adds more realistic examples for dummy data

--- src/constants.ts
+++ src/constants.ts
@@ -5,38 +5,46 @@ export const MAX_CHARACTER_COUNT = 144;
 export const dummy_card_data: Idea[] = [
   {
     id: "1",
-    title: "A",
-    description: "This is card 1",
-    created: new Date(),
-    updated: new Date(),
+    title: "Eco-Friendly Cleaning Service",
+    description:
+      "A cleaning service that uses only environmentally-friendly products and methods.",
+    created: new Date("2020-01-01"),
   },
   {
     id: "2",
-    title: "B",
-    description: "This is card 2",
-    created: new Date(),
-    updated: new Date(),
+    title: "Virtual Wardrobe Organizer",
+    description:
+      "An app that helps users keep track of their clothing inventory, create outfits, and plan their wardrobe.",
+    created: new Date("2020-02-22"),
+    updated: new Date("2020-02-23"),
   },
   {
     id: "3",
-    title: "C",
-    description: "This is card 3",
-    created: new Date(),
-    updated: new Date(),
+    title: "Smart Grocery List",
+    description:
+      "An app that generates a grocery list based on a user's dietary restrictions, meal plans, and favorite recipes. Get healthier today!",
+    created: new Date("2020-03-11"),
   },
   {
     id: "4",
-    title: "D",
-    description: "This is card 4",
-    created: new Date(),
-    updated: new Date(),
+    title: "Mindful Movement Studio",
+    description:
+      "A studio that offers a variety of classes that combine physical movement with mindfulness and meditation practices.",
+    created: new Date("2020-04-02"),
   },
   {
     id: "5",
-    title: "E",
+    title: "E-Bike Rental Service",
     description:
-      "Nam quis nulla. Integer malesuada. In in enim a arcu imperdiet malesuada. Sed vel lectus. Donec odio urna, tempus molestie, porttitor ut, iaculi",
-    created: new Date(),
-    updated: new Date(),
+      "A service that rents electric bikes for daily or weekly use, making it easy for people to try out e-bikes before buying one.",
+    created: new Date("2020-05-15"),
+    updated: new Date("2020-05-16"),
+  },
+  {
+    id: "6",
+    title: "Online Pet Sitting Service",
+    description:
+      "A service that connects pet owners with pet sitters who can watch their pets remotely via webcam.",
+    created: new Date("2020-06-05"),
   },
 ];
